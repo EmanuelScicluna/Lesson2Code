691fd09faef1cc0502b9630c5190111052ba4ebf
nick <nickjohnsonmlt97@gmail.com>
2017-04-20 14:13:04 +0200

    test

---
+++ test.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+<? php 
+
+ function add ($num1,$num2){
+ $num3 = $num1 * $num2;
+ return $num3
+ } $num4 =  add ( add(1445,443),add(33443,3443));
+
+   print $num4;
+
+ ?>
+</body>
+</html>
